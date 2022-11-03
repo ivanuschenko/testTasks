b45c5dac7377177f5d2643cece9f15647db733f8
Ivanuschenko Michael <ivanuschenko.exceedteam@gmail.com>
2022-11-03 14:04:37 +0300

    sb

--- task2.ts
+++ task2.ts
@@ -2,12 +2,15 @@ type CounterType = (count?: number) => [() => number, () => void];
 
 export const counter: CounterType = (count) => {
   let state: number = count ? count : 0;
+
   const getCount = () => {
     return state;
   };
+
   const increment = () => {
     state++;
   };
+
   return [getCount, increment];
 };
 
